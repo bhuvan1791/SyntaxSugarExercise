7b68ddbfb62041e97a585ced7a88428a95444bc3
bkumar408 <bhuwan1791@yahoo.com>
2021-02-25 23:58:15 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var firstName = "Jane";
+            //var lastName = "Doe";
+            //var weight = 150;
+            //Console.WriteLine($"Hello {firstName} your full name is {firstName} {lastName} and you are {weight} LB!");
+
+            Console.WriteLine("Input from user: ");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + " greater than or equal to nine";
+            }
+
+            Console.WriteLine(response);
+
+        }
+    }
+}
